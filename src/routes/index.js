251ef5dfb148afa6952037249e3bf0def6ede64e
769062519 <769062159.com>
2018-11-21 17:44:34 +0800

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import React,{Component,Fragment} from 'react'
 import { Route, Switch, Redirect} from 'react-router-dom'
-import Header from '../components/header'
-import Footer from '../components/footer'
-import Home from '../containers/home'
+import Header from '../common/header'
+import Footer from '../common/footer'
+import Home from '../components/home'
 
 export default ()=>(
     <div>
